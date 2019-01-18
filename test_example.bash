b4599debdeac8138d4ff365f97165e212f6343ee
Beta Faccion <betafcc@gmail.com>
2019-01-18 21:23:25 -0200

    Adds small test based on readme examples

---
+++ test_example.bash
@@ -0,0 +1,17 @@
+source './clc.bash'
+
+clc '\
+<rgb 255 255 255 + bg rgb 235 100 100:\
+  hello<bold: world>,\
+  <reverse: how are >\
+  <underline: you?>  >
+'
+
+PS1=$(clc '\
+<bold:\
+<red:[\
+<yellow:\\u>\
+<green:@>\
+<blue:\\h> \
+<magenta:\\W>\
+]>$ >' | clc_escape)
